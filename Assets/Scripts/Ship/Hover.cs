241ce9a0952e8452835ce07f33fa7e5efcc89c7e
WalterPlinge <22519813+WalterPlinge@users.noreply.github.com>
2019-02-23 21:53:15 +0000

    Created hover script

---
+++ Assets/Scripts/Ship/Hover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Ship
+{
+	public class Hover : MonoBehaviour
+	{
+		public Rigidbody Ship;
+
+		public double TargetHeight;
+		public double MaxDistance;
+		public double Force;
+
+		public PidController Pid;
+
+		// Start is called before the first frame update
+		private void Start()
+		{
+			Ship = GetComponent<Rigidbody>();
+
+			TargetHeight = 1;
+			MaxDistance = 4;
+			Force = 128;
+
+			Pid = new PidController();
+		}
+
+		// Update is called once per frame
+		private void FixedUpdate()
+		{
+			
+		}
+	}
+}
