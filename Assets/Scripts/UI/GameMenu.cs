10748178f6e7dd6998e2f96a4eceac635817e24f
40285303 <35329328+40285303@users.noreply.github.com>
2019-02-24 21:39:05 +0000

    Add UI elements

---
+++ Assets/Scripts/UI/GameMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Close escape menu
+    public void ResumeButton()
+    {
+        SceneManager.UnloadSceneAsync("EscapeMenu");
+    }
+
+
+    // Resets the game
+    public void RestartButton()
+    {
+        SceneManager.UnloadSceneAsync("EscapeMenu");
+
+    }
+
+
+    // Quits the application
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/StartButtonScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        //SceneManager.LoadSceneAsync("Start Button", LoadSceneMode.Additive);
+        //SceneManager.LoadSceneAsync("Menu Background", LoadSceneMode.Additive);
+    }
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+		
+
+	// Loads the main menu scene
+	public void LoadMenuScene()
+	{
+		SceneManager.LoadSceneAsync("Menu Buttons", LoadSceneMode.Additive);
+		SceneManager.UnloadSceneAsync("Start Button");
+	}
+
+
+	// Loads the game scene
+	public void LoadGameScene()
+	{
+		SceneManager.LoadSceneAsync("Space Loop");
+        SceneManager.LoadSceneAsync("HUD", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("Start Button");
+	}
+
+
+	// Quits the application
+	public void QuitButton()
+	{
+		Application.Quit();
+	}
+}
