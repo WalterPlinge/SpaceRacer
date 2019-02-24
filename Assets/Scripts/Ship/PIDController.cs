98f59036033af164fffa9f749e26f8fa44d92c63
WalterPlinge <22519813+WalterPlinge@users.noreply.github.com>
2019-02-24 17:25:47 +0000

    Refactored PID controller

--- Assets/Scripts/Ship/PIDController.cs
+++ Assets/Scripts/Ship/PIDController.cs
@@ -1,5 +1,4 @@
 ﻿// The class handles calculating a desired value based on a PID algorithm
-// This code is not specific to this game and is instead how PID algorithms work in electronics, robotics, and controlling software
 
 using UnityEngine;
 
@@ -8,31 +7,30 @@ namespace Assets.Scripts.Ship
 	[System.Serializable]
 	public class PidController
 	{
-		//Our PID coefficients for tuning the controller
-		public float pCoeff = .8f;
-		public float iCoeff = .0002f;
-		public float dCoeff = .2f;
-		public float minimum = -1;
-		public float maximum = 1;
+		// Our PID coefficients for tuning the controller
+		public float Kp = .8f;
+		public float Ki = .0002f;
+		public float Kd = .2f;
+		public float Min = -1;
+		public float Max = 1;
 
-		//Variables to store values between calculations
-		float integral;
-		float lastProportional;
+		// Variables to store values between calculations
+		private float integral_;
+		private float lastProportional_;
 
-		//We pass in the value we want and the value we currently have, the code
-		//returns a number that moves us towards our goal
+		// Returns value that will move current toward goal
 		public float Seek(float seekValue, float currentValue)
 		{
 			float deltaTime = Time.fixedDeltaTime;
 			float proportional = seekValue - currentValue;
 
-			float derivative = (proportional - lastProportional) / deltaTime;
-			integral += proportional * deltaTime;
-			lastProportional = proportional;
+			float derivative = (proportional - lastProportional_) / deltaTime;
+			integral_ += proportional * deltaTime;
+			lastProportional_ = proportional;
 
 			//This is the actual PID formula. This gives us the value that is returned
-			float value = pCoeff * proportional + iCoeff * integral + dCoeff * derivative;
-			value = Mathf.Clamp(value, minimum, maximum);
+			float value = Kp * proportional + Ki * integral_ + Kd * derivative;
+			value = Mathf.Clamp(value, Min, Max);
 
 			return value;
 		}
