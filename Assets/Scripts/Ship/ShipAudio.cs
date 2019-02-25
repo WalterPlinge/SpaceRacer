f54002738bf21af52e0c61add68f2ecca970e2e3
WalterPlinge <22519813+WalterPlinge@users.noreply.github.com>
2019-02-25 00:16:08 +0000

    Added Audio control script for ship

---
+++ Assets/Scripts/Ship/ShipAudio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Ship;
+using UnityEngine;
+
+public class ShipAudio : MonoBehaviour
+{
+	public float MinVolume = 0.01f;
+	public float MaxVolume = 0.4f;
+	public float MinPitch = 0.3f;
+	public float MaxPitch = 0.8f;
+
+	private ShipMovement movement_;
+	private AudioSource audio_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	    audio_ = GetComponent<AudioSource>();
+	    movement_ = GetComponent<ShipMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if (audio_ == null)
+	    {
+		    audio_ = GetComponent<AudioSource>();
+			if (audio_ == null)
+				return;
+	    }
+
+	    float speed = movement_.GetSpeedPercentage();
+
+	    audio_.volume = Mathf.Lerp(MinVolume, MaxVolume, speed);
+	    audio_.pitch = Mathf.Lerp(MinPitch, MaxPitch, speed);
+    }
+}
