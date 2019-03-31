2af12c4a66c5c78f963c764feb3ade7c5c092555
40285303 <35329328+40285303@users.noreply.github.com>
2019-03-31 20:22:04 +0100

    Added controls menu

--- Assets/Scripts/UI/StartButtonScript.cs
+++ Assets/Scripts/UI/StartButtonScript.cs
@@ -6,23 +6,23 @@ using UnityEngine.SceneManagement;
 public class StartButtonScript : MonoBehaviour
 {
 
-	// Start is called before the first frame update
-	public void Start()
-	{
-		//Loads start button scene if no menu scenes are present
-		if (SceneManager.GetSceneByName("Start Button").isLoaded == false && SceneManager.GetSceneByName("Menu Buttons").isLoaded == false)
-		{
-			SceneManager.LoadSceneAsync("Start Button", LoadSceneMode.Additive);
-		}
-	}
+    // Start is called before the first frame update
+    public void Start()
+    {
+        //Loads start button scene if no menu scenes are present
+        if (SceneManager.GetSceneByName("Start Button").isLoaded == false && SceneManager.GetSceneByName("Menu Buttons").isLoaded == false)
+        {
+            SceneManager.LoadSceneAsync("Start Button", LoadSceneMode.Additive);
+        }
+    }
 
 
-	// Update is called once per frame
-	public void Update()
-	{
+    // Update is called once per frame
+    public void Update()
+    {
 
-	}
-
+    }
+		
 
 	// Loads the main menu scene
 	public void LoadMenuScene()
@@ -32,18 +32,81 @@ public class StartButtonScript : MonoBehaviour
 	}
 
 
-	// Loads the game scene
+	// Loads the race track
 	public void LoadGameScene()
 	{
-		SceneManager.LoadSceneAsync("Space Loop");
-		SceneManager.LoadSceneAsync("HUD", LoadSceneMode.Additive);
-		SceneManager.UnloadSceneAsync("Start Button");
+		SceneManager.LoadSceneAsync("Basic Track");
+        SceneManager.LoadSceneAsync("HUD", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("Start Button");
 	}
 
 
-	// Quits the application
-	public void QuitButton()
+    // Loads controls menu
+    public void LoadControlsScene()
+    {
+        SceneManager.UnloadSceneAsync("Start Button");
+        SceneManager.LoadSceneAsync("Control Menu", LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync("KeyboardImage", LoadSceneMode.Additive);
+       
+    }
+
+
+    // Display keyboard controls
+    public void DisplayKeyboard()
+    {
+        UnloadControls();
+
+        SceneManager.LoadSceneAsync("KeyboardImage");
+    }
+
+
+    // Display playstation controls
+    public void DisplayPS4()
+    {
+        UnloadControls();
+
+        SceneManager.LoadSceneAsync("PS4Image");
+    }
+
+
+    // Display XBOX controls
+    public void DisplayXBOX()
+    {
+        UnloadControls();
+
+        SceneManager.LoadSceneAsync("XBOXImage");
+    }
+
+
+    // Quits the application
+    public void QuitButton()
 	{
 		Application.Quit();
 	}
+
+
+    // Removes currently displayed control scheme
+    public void UnloadControls()
+    {
+        if (SceneManager.GetSceneByName("KeyboardImage").isLoaded == true)
+        {
+            SceneManager.UnloadSceneAsync("KeyboardImage");
+        }
+        else if (SceneManager.GetSceneByName("PS4Image").isLoaded == true)
+        {
+            SceneManager.UnloadSceneAsync("PS4Image");
+        }
+        else if (SceneManager.GetSceneByName("XBOXImage").isLoaded == true)
+        {
+            SceneManager.UnloadSceneAsync("XBOXImage");
+        }
+    }
+
+
+    // Goes back to main menu from controls page
+    public void ControlsBack()
+    {
+        SceneManager.LoadSceneAsync("Menu Background");
+        SceneManager.LoadSceneAsync("Menu Buttons", LoadSceneMode.Additive);
+    }
 }
